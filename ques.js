03d7d5c479bb82040e90ef0d3092c620948d7e71
annusingh58 <127593919+annusingh58@users.noreply.github.com>
2023-04-28 14:10:35 -0400

    updates

---
+++ ques.js
@@ -0,0 +1,29 @@
+// <!-- question 1. -->
+
+var array =[34,45,65,23,5,676,32,34]
+var target =66
+
+for(i=0;i>= array.lenth;i++){ 
+    if(no1+no2==66)  {
+
+    } 
+
+    
+    
+    else{
+        console.log("print the no");
+    } 
+
+    
+
+}
+
+
+// question 2.
+var start=5768;
+var end=78656;
+
+
+
+ 
+
